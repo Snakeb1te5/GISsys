b3079ecd371376a7be5c1dbee7426164e94c9aaf
Snakeb1te5 <kopkov.d@gmail.com>
2025-03-15 19:26:58 +0300

    import Json

---
+++ data/shapes/railwayStations.js
@@ -0,0 +1,17 @@
+var rws = {
+"type": "FeatureCollection",
+"name": "railwayStations",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "Курский вокзал", "address": null, "yearOpen": 1896, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.660934, 55.757059 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "Павелецкий вокзал", "address": null, "yearOpen": 1900, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.639922, 55.729653 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "Киевский вокзал", "address": null, "yearOpen": 1899, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.567279, 55.743124 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "Белорусский вокзал", "address": null, "yearOpen": 1870, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.581345, 55.776762 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "Савёловский вокзал", "address": null, "yearOpen": 1902, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.587997, 55.794321 ] } },
+{ "type": "Feature", "properties": { "id": 6, "name": "Рижский вокзал", "address": null, "yearOpen": 1901, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.632518, 55.792999 ] } },
+{ "type": "Feature", "properties": { "id": 7, "name": "Ленинградский вокзал", "address": null, "yearOpen": 1849, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.655087, 55.776467 ] } },
+{ "type": "Feature", "properties": { "id": 8, "name": "Ярославский вокзал", "address": null, "yearOpen": 1880, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.657224, 55.776679 ] } },
+{ "type": "Feature", "properties": { "id": 9, "name": "Казанский вокзал", "address": null, "yearOpen": 1862, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.656372, 55.77337 ] } },
+{ "type": "Feature", "properties": { "id": 10, "name": "Восточный вокзал", "address": null, "yearOpen": 2021, "platf_qty": null }, "geometry": { "type": "Point", "coordinates": [ 37.744961, 55.802752 ] } }
+]
+}
