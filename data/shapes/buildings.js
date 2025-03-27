e9896f52e4d1ac6383727c5ff71362d6c9348dd9
Snakeb1te5 <kopkov.d@gmail.com>
2025-03-27 22:30:12 +0300

    photo statinons

---
+++ data/shapes/buildings.js
@@ -0,0 +1,9 @@
+var buildings = {
+"type": "FeatureCollection",
+"name": "buildings",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "adress": "105064 г. Москва гороховский преулок 4 к.3", "material": "кирпич" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 37.661284, 55.763991 ], [ 37.661623, 55.763989 ], [ 37.661623, 55.763836 ], [ 37.661282, 55.763852 ], [ 37.661284, 55.763991 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": 2, "adress": "105064 г. Москва гороховский переулок 4 стр4", "material": "железобетон" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 37.660774, 55.76335 ], [ 37.660608, 55.763558 ], [ 37.660962, 55.76366 ], [ 37.661108, 55.763469 ], [ 37.660774, 55.76335 ] ] ] ] } }
+]
+}
--- data/shapes/railwayStations.js
+++ data/shapes/railwayStations.js
@@ -1,4 +1,4 @@
-var rws = {
+var rwss = {
 "type": "FeatureCollection",
 "name": "railwayStations",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
